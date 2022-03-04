a9619ddb02b4e233746cb58aefb040468e2ac598
JoeBuydemDips <ksarkodie111@icloud.com>
2022-03-04 11:40:54 -0500

    Add decrease cartitem amount.

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,16 @@
 const reducer = (state, action) => {
+  //clear cart function
   if (action.type == "CLEAR_CART") {
     return { ...state, cart: [] };
   }
+  //Remove item function
   if (action.type == "REMOVE") {
     return {
       ...state,
       cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
     };
   }
+  //increase item function
   if (action.type == "INCREASE") {
     let tempCart = state.cart.map((cartItem) => {
       if (cartItem.id === action.payload) {
@@ -15,6 +18,19 @@ const reducer = (state, action) => {
       }
       return cartItem;
     });
+
+    return { ...state, cart: tempCart };
+  }
+  //decrease item function
+  if (action.type == "DECREASE") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount != 0);
 
     return { ...state, cart: tempCart };
   }
